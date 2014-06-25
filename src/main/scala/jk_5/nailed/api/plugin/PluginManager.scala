ac6ddc0027403bb7bdf9fd1f24acd99d34fdf0fa
jk-5 <jeffreykog@me.com>
2014-06-25 07:17:06 +0200

    Actual plugin loading

--- src/main/scala/jk_5/nailed/api/plugin/PluginManager.scala
+++ src/main/scala/jk_5/nailed/api/plugin/PluginManager.scala
@@ -1,13 +1,17 @@
 package jk_5.nailed.api.plugin
 
+import java.io.{File, InputStream, InputStreamReader}
 import java.util
+import java.util.jar.JarFile
 import java.util.regex.Pattern
 
 import com.google.common.collect.{ArrayListMultimap, Multimap}
+import com.google.gson.Gson
 import jk_5.eventbus.EventBus
 import jk_5.nailed.api.NailedServer
 import jk_5.nailed.api.chat.{ChatColor, ComponentBuilder, TabExecutor}
 import jk_5.nailed.api.command.CommandSender
+import org.apache.commons.lang3.Validate
 import org.apache.logging.log4j.LogManager
 
 import scala.collection.mutable
@@ -21,10 +25,12 @@ class PluginManager(private val server: NailedServer) {
 
   private val eventBus = new EventBus
   private val plugins = mutable.HashMap[String, Plugin]()
+  private val toLoad = mutable.HashMap[String, PluginDescription]()
   private val commandMap = mutable.HashMap[String, Command]()
   private val commandsByPlugin: Multimap[Plugin, Command] = ArrayListMultimap.create()
   private val argsSplit = Pattern.compile(" ")
   private val logger = LogManager.getLogger
+  private val gson = new Gson
 
   /**
    * Register a command so that it may be executed.
@@ -104,4 +110,109 @@ class PluginManager(private val server: NailedServer) {
    * @return the retrieved plugin or null if not loaded
    */
   def getPlugin(name: String) = this.plugins.get(name).orNull
+
+  /**
+   * Load all plugins from the specified folder.
+   *
+   * @param folder the folder to search for plugins in
+   */
+  def discoverPlugins(folder: File){
+    Validate.notNull(folder, "folder")
+    Validate.validState(folder.isDirectory, "Must load from a directory")
+
+    for(file <- folder.listFiles()){
+      if(file.isFile && file.getName.endsWith(".jar")){
+        var jar: JarFile = null
+        try{
+          jar = new JarFile(file)
+          val pdf = jar.getJarEntry("plugin.json")
+          Validate.notNull(pdf, "Plugin must have a plugin.json file")
+
+          var is: InputStream = null
+          try{
+            is = jar.getInputStream(pdf)
+            val desc = gson.fromJson(new InputStreamReader(is), classOf[PluginDescription])
+            desc.setFile(file)
+            toLoad.put(desc.getName, desc)
+          }finally{
+            is.close()
+          }
+        }catch{
+          case e: Exception => logger.warn("Could not load plugin from file " + file, e)
+        }finally{
+          if(jar != null) jar.close()
+        }
+      }
+    }
+  }
+
+  def loadPlugins(){
+    val pluginStatuses = mutable.HashMap[PluginDescription, java.lang.Boolean]()
+    for(e <- toLoad){
+      val plugin = e._2
+      if(!enablePlugin(pluginStatuses, new mutable.Stack[PluginDescription](), plugin)){
+        logger.warn("Failed to enable " + e._1)
+      }
+    }
+    toLoad.clear()
+    //toLoad = null
+  }
+
+  def enablePlugins(){
+    for(plugin <- plugins.values){
+      try{
+        plugin.onEnable()
+        logger.info("Enabled plugin {0} version {1} by {2}", plugin.getDescription.getName, plugin.getDescription.getVersion, plugin.getDescription.getAuthor)
+      }catch{
+        case e: Exception => logger.warn("Exception encountered when loading plugin: " + plugin.getDescription.getName, e)
+      }
+    }
+  }
+
+  def enablePlugin(pluginStatuses: mutable.HashMap[PluginDescription, java.lang.Boolean], dependencyStack: mutable.Stack[PluginDescription], plugin: PluginDescription): Boolean = {
+    if(pluginStatuses.contains(plugin)) pluginStatuses.get(plugin).get
+    val dependencies = mutable.HashSet[String]()
+    dependencies ++= plugin.getDepends
+    dependencies ++= plugin.getSoftDepends
+    var status = true
+
+    for(dependencyName <- dependencies if status){
+      val depend = toLoad.get(dependencyName)
+      var dependStatus: java.lang.Boolean = if(depend.isDefined) pluginStatuses.get(depend.get).orNull else java.lang.Boolean.FALSE
+      if(dependStatus == null){
+        if(dependencyStack.contains(depend)){
+          val dependencyGraph = new StringBuilder
+          for(element <- dependencyStack){
+            dependencyGraph.append(element.getName).append(" -> ")
+          }
+          dependencyGraph.append(plugin.getName).append(" -> ").append(dependencyName)
+          logger.warn("Circular dependency detected: " + dependencyGraph)
+          status = false
+        }else{
+          dependencyStack.push(plugin)
+          dependStatus = this.enablePlugin(pluginStatuses, dependencyStack, depend.get)
+          dependencyStack.pop()
+        }
+      }
+      if(dependStatus == java.lang.Boolean.FALSE && plugin.getDepends.contains(dependencyName)){ //Only fail if this wasn't a soft dependency
+        logger.warn("{0} (required by {1}) is unavailable", dependencyName, plugin.getName)
+        status = false
+      }
+    }
+
+    if(status) try{
+      val loader = new PluginClassLoader(Array(plugin.getFile.toURI.toURL))
+      val main = loader.loadClass(plugin.getMain)
+      val cl = main.getDeclaredConstructor().newInstance().asInstanceOf[Plugin]
+      cl.init(server, plugin)
+      plugins.put(plugin.getName, cl)
+      cl.onLoad()
+      logger.info("Loaded plugin {0} version {1} by {2}", plugin.getName, plugin.getVersion, plugin.getAuthor)
+    }catch{
+      case t: Throwable =>
+        logger.warn("Error loading plugin " + plugin.getName, t)
+    }
+    pluginStatuses.put(plugin, status)
+    status
+  }
 }
