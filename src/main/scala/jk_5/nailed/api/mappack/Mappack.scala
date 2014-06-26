1bf5905a63fe984e32ceca51439f8ee17aaab9be
jk-5 <jeffreykog@me.com>
2014-06-26 17:35:13 +0200

    Added 2 mappack traits. Far from done. Have to finish the scheduler first

---
+++ src/main/scala/jk_5/nailed/api/mappack/Mappack.scala
@@ -0,0 +1,11 @@
+package jk_5.nailed.api.mappack
+
+/**
+ * No description given
+ *
+ * @author jk-5
+ */
+trait Mappack {
+  def getId: String
+  def getMetadata: MappackMetadata
+}
---
+++ src/main/scala/jk_5/nailed/api/mappack/MappackMetadata.scala
@@ -0,0 +1,10 @@
+package jk_5.nailed.api.mappack
+
+/**
+ * No description given
+ *
+ * @author jk-5
+ */
+trait MappackMetadata {
+  def getName: String
+}
