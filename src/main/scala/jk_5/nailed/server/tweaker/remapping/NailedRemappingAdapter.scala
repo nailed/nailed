6ea9f43f2617d12bdb9bd03c8fb7f78dd7d572ed
jk-5 <jeffreykog@me.com>
2014-07-17 12:22:01 +0200

    Properly merge super access maps

--- src/main/scala/jk_5/nailed/server/tweaker/remapping/NailedRemappingAdapter.scala
+++ src/main/scala/jk_5/nailed/server/tweaker/remapping/NailedRemappingAdapter.scala
@@ -1,7 +1,7 @@
 package jk_5.nailed.server.tweaker.remapping
 
-import org.objectweb.asm.ClassVisitor
-import org.objectweb.asm.commons.RemappingClassAdapter
+import org.objectweb.asm.commons.{Remapper, RemappingClassAdapter, RemappingMethodAdapter}
+import org.objectweb.asm.{ClassVisitor, MethodVisitor, Opcodes}
 
 /**
  * No description given
@@ -9,5 +9,35 @@ import org.objectweb.asm.commons.RemappingClassAdapter
  * @author jk-5
  */
 class NailedRemappingAdapter(visitor: ClassVisitor) extends RemappingClassAdapter(visitor, NameRemapper) {
+  
+  override def visit(version: Int, access: Int, name: String, signature: String, superName: String, interfaces: Array[String]){
+    val i = if(interfaces == null) new Array[String](0) else interfaces
+    NameRemapper.mergeSuperMaps(name, superName, interfaces)
+    super.visit(version, access, name, signature, superName, interfaces)
+  }
 
+  override protected def createRemappingMethodAdapter(access: Int, desc: String, mv: MethodVisitor): MethodVisitor = {
+    new StaticFixingMethodVisitor(access, desc, mv, remapper)
+  }
+
+  class StaticFixingMethodVisitor(access: Int, desc: String, mv: MethodVisitor, remapper: Remapper) extends RemappingMethodAdapter(access, desc, mv, remapper) {
+    
+    override def visitFieldInsn(opcode: Int, originalType: String, originalName: String, desc: String){
+      //This MethodVisitor solves the problem of a static field reference changing type.
+      //Chances are big that this change is compatible, however we need to fix up the descriptor to point at the new type
+      val typ = remapper.mapType(originalType)
+      val fieldName = remapper.mapFieldName(originalType, originalName, desc)
+      var newDesc = remapper.mapDesc(desc)
+      if(opcode == Opcodes.GETSTATIC && typ.startsWith("net/minecraft/") && newDesc.startsWith("Lnet/minecraft/")){
+        val replDesc = NameRemapper.getStaticFieldType(originalType, originalName, typ, fieldName)
+        if(replDesc != null){
+          newDesc = remapper.mapDesc(replDesc)
+        }
+      }
+      //super.super
+      if(mv != null){
+        mv.visitFieldInsn(opcode, typ, fieldName, newDesc)
+      }
+    }
+  }
 }
