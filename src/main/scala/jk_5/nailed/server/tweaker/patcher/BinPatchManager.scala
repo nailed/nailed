9f27e535f7685021c32f6c6ef8934cf8e7692472
jk-5 <jeffreykog@me.com>
2014-08-25 15:09:04 +0200

    Add copyright notice to all files

--- src/main/scala/jk_5/nailed/server/tweaker/patcher/BinPatchManager.scala
+++ src/main/scala/jk_5/nailed/server/tweaker/patcher/BinPatchManager.scala
@@ -1,13 +1,30 @@
+/*
+ * Nailed, a Minecraft PvP server framework
+ * Copyright (C) jk-5 <http://github.com/jk-5/>
+ * Copyright (C) Nailed team and contributors <http://github.com/nailed/>
+ *
+ * This program is free software: you can redistribute it and/or modify it
+ * under the terms of the MIT License.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License
+ * for more details.
+ *
+ * You should have received a copy of the MIT License along with
+ * this program. If not, see <http://opensource.org/licenses/MIT/>.
+ */
+
 package jk_5.nailed.server.tweaker.patcher
 
-import java.io.{ByteArrayOutputStream, ByteArrayInputStream, IOException, File}
-import java.util.jar.{JarEntry, JarOutputStream, Pack200, JarInputStream}
+import java.io.{ByteArrayInputStream, ByteArrayOutputStream, File, IOException}
+import java.util.jar.{JarEntry, JarInputStream, JarOutputStream, Pack200}
 import java.util.regex.Pattern
 
 import LZMA.LzmaInputStream
 import com.google.common.collect.{ArrayListMultimap, ListMultimap}
 import com.google.common.hash.Hashing
-import com.google.common.io.{ByteStreams, ByteArrayDataInput, Files}
+import com.google.common.io.{ByteArrayDataInput, ByteStreams, Files}
 import com.nothome.delta.GDiffPatcher
 import net.minecraft.launchwrapper.LaunchClassLoader
 import org.apache.logging.log4j.LogManager
