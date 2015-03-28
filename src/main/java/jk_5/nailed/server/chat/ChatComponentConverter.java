9abce6f7c7f81cfba2abaaeaf48b0c53b2766ec6
jk-5 <jeffreykog@me.com>
2015-03-28 13:22:55 +0100

    Added new chat api and deprecated the old one

--- src/main/java/jk_5/nailed/server/chat/ChatComponentConverter.java
+++ src/main/java/jk_5/nailed/server/chat/ChatComponentConverter.java
@@ -5,6 +5,7 @@ import net.minecraft.util.*;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
+@Deprecated
 public class ChatComponentConverter {
 
     private static final Logger logger = LogManager.getLogger();
